d5c28d3931a6d171abe9b4d764dedbabfaa968b0
iq9891 <649076408@qq.com>
2021-07-02 20:29:21 +0800

    chore($remove): remove vue files

--- components/table-pro/src/components/aTableTitle.vue
+++
@@ -1,8 +0,0 @@
-<!-- @format -->
-
-<template>
-  <BasicTitle v-if="getTitle" :help-message="helpMessage">
-    {{ getTitle }}
-  </BasicTitle>
-</template>
-<script lang="ts" src="./table-title.ts"></script>
--- components/table-pro/src/components/settings/aIndex.vue
+++
@@ -1,9 +0,0 @@
-<!-- @format -->
-
-<template>
-  <div :class="prefixClsNew">
-    <ReloadSetting v-if="getSetting.reload" />
-    <ColumnSetting v-if="getSetting.setting" />
-  </div>
-</template>
-<script lang="ts" src="./index.ts"></script>
