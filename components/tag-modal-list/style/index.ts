0152df405a63cc08509a7afa87530bcd0d6772f2
iq9891 <649076408@qq.com>
2022-01-10 10:42:36 +0800

    refactor($icon): use cube for modal user

--- components/tag-modal-list/style/index.ts
+++ components/tag-modal-list/style/index.ts
@@ -3,5 +3,6 @@ import '../../basic-arrow/style';
 import '../../tag-group/style';
 import '../../button/style';
 import '../../empty/style';
+import '../../spin/style';
 import '../../modal-pro/style';
 import './index.less';
