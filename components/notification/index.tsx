b5410cb36ba3a05a7f92bebc25067b8ed14ef5da
天意 <649076408@qq.com>
2022-02-22 10:05:20 +0800

    fix($notification): remove async

--- components/notification/index.tsx
+++ components/notification/index.tsx
@@ -277,7 +277,7 @@ export interface NotificationApi extends NotificationInstance {
 }
 
 /** @private test Only function. Not work on production */
-export const getInstance = async (cacheKey: string) =>
+export const getInstance = (cacheKey: string) =>
   process.env.NODE_ENV === 'test' ? notificationInstance[cacheKey] : null;
 
 export default api as NotificationApi;
