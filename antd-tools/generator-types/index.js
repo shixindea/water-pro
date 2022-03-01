fbb0259e5dc48c9b3dfd66c219491f67c367d7f0
天意 <649076408@qq.com>
2022-03-01 22:38:51 +0800

    update($npm): vue

--- antd-tools/generator-types/index.js
+++ antd-tools/generator-types/index.js
@@ -7,8 +7,8 @@ const rootPath = path.resolve(__dirname, '../../');
 try {
   parseAndWrite({
     version: pkg.version,
-    name: 'types',
-    path: path.resolve(rootPath, './v2-doc/src/docs'),
+    name: '@fe6/water-pro',
+    path: path.resolve(rootPath, './components'),
     // default match lang
     test: /en-US\.md/,
     outputDir: path.resolve(rootPath, './vetur'),
